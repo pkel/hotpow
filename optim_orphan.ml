f620cc0a0dcf3e60c78222e0deca4a968aa68987
Patrik Keller <git@pkel.dev>
2020-11-03 18:31:41 +0100

    Another hand-coded optimization which does not work

--- optim_orphan.ml
+++ optim_orphan.ml
@@ -7,19 +7,13 @@ open Base
 let tag = "optim-orphan"
 let quorum_size = 32
 let target_orphan_rate = 0.01
-let n_nodes = 100
-let n_blocks = 2000
-let n_cores = Cpu.numcores ()
 
-let population_size = (4, 4)
 let min_pow_scale = 0.25
 let max_pow_scale = 1. /. min_pow_scale /. target_orphan_rate
 
-type chromosome = { pow_scale: float; fitness: float; female: bool }
-
-let fitness orphan_rate =
-  let open Float in
-  1. /. ((orphan_rate -. target_orphan_rate) ** 2.)
+let n_nodes = 15
+let n_blocks = 500
+let n_cores = Cpu.numcores ()
 
 let par_sim n_cores n_times io params =
   let n = ref n_times in
@@ -60,76 +54,80 @@ let orphan_rate pow_scale =
         alpha= 1 // n_nodes
       ; strategy= Parallel
       } in
-    par_sim n_cores n_cores  io_params params
+    par_sim n_cores n_cores io_params params
   in
   let () =
-    Stdio.printf ". yields orphan rate %05.4f and fitness %.0f.\n%!" orphan_rate (fitness orphan_rate)
+    Stdio.printf ". yields orphan rate %05.4f.\n%!" orphan_rate
   in
   orphan_rate
 
-let chromosome pow_scale =
-  let orphan_rate = orphan_rate pow_scale in
-  let female =
-    if Float.is_negative (orphan_rate -. target_orphan_rate) then true else false
+let max_pow_scale_point = (max_pow_scale, orphan_rate max_pow_scale)
+let min_pow_scale_point = (min_pow_scale, orphan_rate min_pow_scale)
+
+let window ~size ~around d =
+  let above =
+    Map.to_sequence
+      ~order:`Increasing_key
+      ~keys_greater_or_equal_to:around
+      d
+  and below =
+    Map.to_sequence
+      ~order:`Decreasing_key
+      ~keys_less_or_equal_to:around
+      d
   in
-  { pow_scale; fitness = fitness orphan_rate; female}
-
-let random () =
-  Random.float_range min_pow_scale max_pow_scale
-
-type population = {female: chromosome array; male: chromosome array}
-
-let population =
-  let female =
-    let c = chromosome max_pow_scale in
-    let () =
-      if not c.female then failwith "max.pow.scale did not produce female"
-    in
-    Array.create ~len:(fst population_size) c
-  and male =
-    let c = chromosome min_pow_scale in
-    let () =
-      if c.female then failwith "min.pow.scale did not produce male"
-    in
-    Array.create ~len:(snd population_size) c
+  let rec collect n above below acc =
+    let h (min, max, keys, values) (k, v) = Float.min min k, Float.max max k, k :: keys, v :: values in
+    if n > 0 then
+      match Sequence.(next above, next below) with
+      | Some (a, above), Some (b, below) ->
+        collect (n-2) above below (h (h acc b) a)
+      | Some (a, above), None ->
+        collect (n-2) above below (h (h acc min_pow_scale_point) a)
+      | None, Some (b, below) ->
+        collect (n-2) above below (h (h acc b) max_pow_scale_point)
+      | None, None ->
+        collect (n-2) above below (h (h acc min_pow_scale_point) max_pow_scale_point)
+    else acc
   in
-  { male; female}
-
-let random_index arr =
-  Random.int (Array.length arr)
-
-let random_element arr =
-  arr.(random_index arr)
-
-let fit_element arr =
-  let overall_fitness = Array.fold ~init:0. ~f:(fun acc c -> c.fitness +. acc) arr in
-  let i = ref (random_index arr) in
-  let pick = Random.float overall_fitness in
-  let passed = ref 0. in
-  let m = Array.length arr in
-  while Float.is_negative (!passed -. pick) do
-    passed := !passed +. arr.(!i).fitness;
-    i := (!i + 1) % m
-  done;
-  arr.(!i)
-
-let crossover () =
-  let f = fit_element population.female
-  and m = fit_element population.male
+  let min, max, key, values =
+    collect size above below (Float.infinity, Float.neg_infinity, [], [])
   in
-  let a = Random.float 1. in
-  let pow_scale = (a *. f.pow_scale +. (1. -. a) *. m.pow_scale) in
-  pow_scale
+  min, max, List.zip_exn key values
 
-let replace (c: chromosome) =
-  let arr = if c.female then population.female else population.male in
-  arr.(random_index arr) <- c
+type line = {intercept: float; slope:float}
 
-let mutate _chromosome = random ()
+let ols points : line =
+  let n, xsum, ysum, xysum, x2sum =
+    List.fold_left points
+      ~f:(fun (n, xsum, ysum, xysum, x2sum) (x,y) -> n + 1, x +. xsum, y +. ysum, x *. y +. xysum, (x **. 2.) +. x2sum)
+      ~init:(0, 0.,0.,0., 0.)
+  in
+  let n = Float.of_int n in
+  let xmean = xsum /. n in
+  let slope =
+    let a = xysum -. xmean *. ysum
+    and b = x2sum -. n *. (xmean **. 2.)
+    in a /. b
+  in
+  let intercept =
+    ysum /. n -. slope *. xmean
+  in {slope; intercept}
 
-let () =
-  for _i = 1 to 100 do
-    crossover ()
-    |> chromosome
-    |> replace
-  done
+let intersect ~y line = (y -. line.intercept) /. line.slope
+
+let optim () =
+  let init = Map.empty (module Float) in
+  let rec step n last_estimate d =
+    Stdio.printf "estimate: %f\n%!" last_estimate;
+    if n > 0 then
+      let min, max, points = window ~around:last_estimate ~size:50 d in
+      let pow_scale = Random.float_range min max in
+      let orphan_rate = orphan_rate pow_scale in
+      let d = Map.set ~key:pow_scale ~data:orphan_rate d in
+      let line = ols ((pow_scale, orphan_rate) :: points) in
+      let next_estimate = intersect ~y:target_orphan_rate line in
+      step (n - 1) next_estimate d
+  in step 1000 ((min_pow_scale +. max_pow_scale) /. 2.) init
+
+let () = optim ()
